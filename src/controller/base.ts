51ef739d090d9541008222a713101fb6dcf0b899
unknown <henriquesantos.uefa@gmail.com>
2021-08-07 20:11:27 -0300

    refactor: adiciona verificação com o metodo valid

--- src/controller/base.ts
+++ src/controller/base.ts
@@ -1,29 +1,22 @@
 import { Request } from 'express';
 import { NotificacaoBase } from "../entity/NotificacaoBase";
 
-export abstract class BaseControlador<T> extends NotificacaoBase{
+export abstract class BaseControlador<T> extends NotificacaoBase {
 
     // private _repository: Repository<T>
 
-    constructor(entidade: any){
+    constructor(entidade: any) {
         super()
         // this._repository = getRepository<T>(entidade)
     }
 
     salvar(modelo: any) {
-        console.log("reposta do modelo::", modelo)
-        if(!modelo.titulo) {
-            return {
-                codigoStatus: 400,
-                corpo: this.notifications
-            }
-        }
 
-        if(!modelo.imagem) {
-            return {
-                codigoStatus: 400,
-                corpo: this.notifications
-            }
+        if (this.valid()) return { message: "Salvo com sucesso" }
+
+        else return {
+            codigoStatus: 400,
+            corpo: this.notifications
         }
     }
 }
