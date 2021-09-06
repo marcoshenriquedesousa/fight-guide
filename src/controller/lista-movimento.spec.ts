265812ce4fd663b4c8c98a8b7464db723daa3352
unknown <henriquesantos.uefa@gmail.com>
2021-09-06 11:15:28 -0300

    feat: Salvar um jogo para gerar um uid valido e Salvar um personagem para gerar um uid valido

--- src/controller/lista-movimento.spec.ts
+++ src/controller/lista-movimento.spec.ts
@@ -1,5 +1,7 @@
 import { criaTypeOemCom } from "../../utils/criaTypeOrmCon"
+import { JogoController } from "./jogo"
 import { ListaDeMovimentoController } from "./lista-movimento"
+import { PersonagemController } from "./personagem"
 
 describe('ListaDeMovimentosController', () => { 
 
@@ -8,19 +10,26 @@ describe('ListaDeMovimentosController', () => {
     })
     
     let dadosSalvos = {
-        uid: '',
+        uidJogo: '',
+        uidPersonagem: '',
         createAt: '',
         updateAt: ''
     }
     
     interface TipoStub {
-        sut: ListaDeMovimentoController
+        sut: ListaDeMovimentoController,
+        jogo: JogoController,
+        personagem: PersonagemController
     }
     
     const constroiSut = (): TipoStub => {
         const sut = new ListaDeMovimentoController()
+        const jogo = new JogoController()
+        const personagem = new PersonagemController()
         return {
             sut,
+            jogo,
+            personagem
         }
     }
   
@@ -78,5 +87,58 @@ describe('ListaDeMovimentosController', () => {
         const respostaHttp = await sut.SalvarLista(requisicaoHttp)
         expect(respostaHttp.codigoStatus).toBe(400)
         expect(respostaHttp.body.mensagem).toEqual('falta o parametro: personagem')
+    })
+
+    test('Salvar um jogo para gerar um uid valido', async () => {
+        const { jogo } = constroiSut()
+        const requisicaoHttp = {
+            body: {
+                titulo: 'titulo_valido',
+                imagem: 'imagem_valida'
+            }
+        }
+        const respostaHttp = await jogo.salvarJogo(requisicaoHttp)
+        dadosSalvos.uidJogo = respostaHttp.body.uid
+        const createAt = respostaHttp.body.createAt
+        const updateAt = respostaHttp.body.updateAt
+        expect(respostaHttp.codigoStatus).toBe(200)
+        expect(respostaHttp.body).toEqual({
+            uid: dadosSalvos.uidJogo,
+            titulo: 'titulo_valido',
+            imagem: 'imagem_valida',
+            deleted: false,
+            createAt: createAt,
+            updateAt: updateAt
+        })
+    })
+
+    test('Salvar um personagem para gerar um uid valido', async () => {
+        const { personagem } = constroiSut()
+        const requisicaoHttp = {
+            body: {
+                nome: 'nome_valido',
+                sobreNome: 'sobreNome_valido',
+                imagem: 'imagem_valida',
+                listaMovimento: 'listaMovimeto_valido',
+                jogo: dadosSalvos.uidJogo
+            }
+        }
+        const respostaHttp = await personagem.salvarPersonagem(requisicaoHttp)
+        dadosSalvos.uidPersonagem = respostaHttp.body.uid 
+        const creatAt = respostaHttp.body.createAt
+        const updateAt = respostaHttp.body.updateAt
+        expect(respostaHttp.codigoStatus).toBe(200)
+        expect(respostaHttp.body).toEqual(
+        {    
+            uid: dadosSalvos.uidPersonagem,
+            nome: 'nome_valido',
+            sobreNome: 'sobreNome_valido',
+            imagem: 'imagem_valida',
+            listaMovimento: 'listaMovimeto_valido',
+            jogo: dadosSalvos.uidJogo,
+            deleted: false,
+            createAt: creatAt,
+            updateAt: updateAt
+        })
     })
 })
